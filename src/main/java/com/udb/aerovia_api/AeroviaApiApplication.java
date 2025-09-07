6eefae14e623e0195d9d61b1981ac5a6798a36ce
Gerardo Saz <gerardo.saz120@gmail.com>
2025-09-06 18:07:41 -0600

    init

---
+++ src/main/java/com/udb/aerovia_api/AeroviaApiApplication.java
@@ -0,0 +1,13 @@
+package com.udb.aerovia_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeroviaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeroviaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udb/aerovia_api/AeroviaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udb.aerovia_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeroviaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
